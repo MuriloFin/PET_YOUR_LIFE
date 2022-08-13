5269502b9bd4bae6f16a8c9d1d8f93613f4fb3fa
murilo finardi <favorettofinardi@gmail.com>
2022-08-13 17:26:07 -0300

    homepage criada com varias features

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
+    if params[:query]
+      @pets = Pet.global_search(params[:query])
+    else
+      # @pets = policy_scope(Pet)
+      @pets = Pet.all
+    end
   end
 end
