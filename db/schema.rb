3d2556de747d8ff95fb4b9c62489eb4a961528b4
Lucas De Oliveira <lucas.deoliveira2112@gmail.com>
2022-08-18 21:01:27 -0300

    fix register pet form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_18_211413) do
+ActiveRecord::Schema.define(version: 2022_08_18_235921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 2022_08_18_211413) do
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "pet_type"
     t.string "breed"
     t.string "colour"
     t.string "size"
