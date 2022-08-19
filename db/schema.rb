c7126f6ff5deb86b9159f0c1bc7b86f4437be9bd
Andre Fonseca <decobf15@gmail.com>
2022-08-18 22:14:30 -0300

    pets show almost done

--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2022_08_18_211413) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
+    t.string "image"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
