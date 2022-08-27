f2d11c6c7aceca23b23c94cb6783d03a8d560202
murilo finardi <favorettofinardi@gmail.com>
2022-08-27 11:43:14 -0300

    solving chat problems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   #config.action_cable.mount_path = "/cable"
   config.action_cable.url = 'wss://pet-your-life.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://pet-your-life.herokuapp.com/', 'https://www.petyourlife.org/cable', /http:\/\/pet-your-life.herokuapp.com.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://pet-your-life.herokuapp.com/', 'https://www.petyourlife.org', /http:\/\/pet-your-life.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
