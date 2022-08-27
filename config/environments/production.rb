2c95ab98d1adc5f1c06daedfe1a7fba2ad2de5af
murilo finardi <favorettofinardi@gmail.com>
2022-08-27 11:04:49 -0300

    solving chat problems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,9 +41,9 @@ Rails.application.configure do
   config.active_storage.service = :cloudinary
 
   # Mount Action Cable outside main process or domain.
-   #config.action_cable.mount_path = "/cable"
-   config.action_cable.url = 'wss://www.petyourlife.org/'
-   config.action_cable.allowed_request_origins = [ 'https://www.petyourlife.org/', /https:\/\/petyourlife.org/.*/ ]
+  #config.action_cable.mount_path = "/cable"
+  config.action_cable.url = 'wss://www.petyourlife.org/'
+  config.action_cable.allowed_request_origins = [ 'https://www.petyourlife.org/', /https:\/\/petyourlife.org.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
