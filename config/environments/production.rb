9a11eab22bdd9027d0c3bd779aa550335f48e2fe
murilo finardi <favorettofinardi@gmail.com>
2022-09-03 18:22:39 -0300

    preparando pro heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   #config.action_cable.mount_path = "/cable"
   config.action_cable.url = 'wss://pet-your-life.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://pet-your-life.herokuapp.com/', 'https://www.petyourlife.org', /http:\/\/pet-your-life.herokuapp.com.*/ ]
-  Rails.application.routes.default_url_options[:host] = “petyourlife.org”
+  # Rails.application.routes.default_url_options[:host] = “petyourlife.org”
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
